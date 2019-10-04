a613efcd7e8cab6c6ec4578c51a10aaeea8a71c0
Patrick <patrickwharton1@gmail.com>
2019-10-04 11:34:27 +0100

    First c++ files

---
+++ week1/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  printf("%s\n", "Hello, world");
+  return 0;
+}
---
+++ week1/user_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  cout << "Enter a number:" << '\n';
+  int age;
+  cin >> age;
+  cout << "You entered " << age << "\n";
+  return age;
+}
